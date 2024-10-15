ba09dddbb7b4b437f2cd9890381c82988d1c9ea3
Sam. <swayams24@gmail.com>
2024-10-16 02:05:44 +1100

    Add constants and Hero headline

---
+++ src/constants/index.jsx
@@ -0,0 +1,10 @@
+export const hero = [
+  {
+    title: "Designing Spaces, Defining Visions",
+    description: "We create space where vision and functionality converge.",
+    button: {
+      title: "get a quote",
+      icon: "./icons/chevron-right.svg",
+    },
+  },
+];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import { hero } from "../constants";
 
 const Hero = () => {
   return (
-    <section className="bg-[url('/images/heroImage.webp')] h-screen w-full bg-cover bg-bottom"></section>
+    <section className="bg-[url('/images/heroImage.webp')] h-screen w-full bg-cover bg-bottom">
+      <NavBar />
+      {hero.map(({ title, description, button }) => (
+        <div className="grid place-content-center h-[60vh] text-center gap-y-4">
+          <h1 className="font-satoshi text-[3.5rem] leading-tight font-normal">
+            {title}
+          </h1>
+          <p className="font-satoshi text-xl font-light">{description}</p>
+          <div className="mx-auto">
+            <button className="font-satoshi text-xl font-light border-[1px] rounded-full border-black px-5 py-2 flex items-center">
+              {button.title}
+              <img src={button.icon} alt="" />
+            </button>
+          </div>
+        </div>
+      ))}
+    </section>
   );
 };
 
