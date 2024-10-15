88c1547f517d299149382ca748ded5682cac19c8
Sam. <swayams24@gmail.com>
2024-10-16 03:46:51 +1100

    Add framer motion and create animation for links

--- src/components/FlipLinks.jsx
+++ src/components/FlipLinks.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const FlipLinks = ({ children, href }) => {
   return (
-    <a href={href} className="text-2xl font-cabinet font-light max-lg:text-xl">
-      {children}
-    </a>
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      className="relative block overflow-hidden whitespace-nowrap text-2xl font-cabinet font-light max-lg:text-xl"
+    >
+      <motion.div
+        variants={{
+          initial: {
+            y: 0,
+          },
+          hovered: {
+            y: "-100%",
+          },
+        }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        className="absolute inset-0"
+        variants={{
+          initial: {
+            y: "100%",
+          },
+          hovered: {
+            y: 0,
+          },
+        }}
+      >
+        {children}
+      </motion.div>
+    </motion.a>
   );
 };
 
