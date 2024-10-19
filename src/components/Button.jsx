ab959468d3aedcf1f1ccdf556d79eca4a2d33bc8
Sam. <swayams24@gmail.com>
2024-10-20 00:09:35 +1100

    Fixed Button component, hover works

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,20 +3,17 @@ import { motion } from "framer-motion";
 import FlipLinks from "./FlipLinks";
 
 const Button = ({ button, href }) => {
-  const [isHovered, setIsHovered] = useState(false);
   return (
-    <motion.button
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="border-[1px] rounded-full border-black px-5 py-2 flex items-center justify-center gap-2 overflow-hidden relative"
-    >
+    <button className="border-[1px] rounded-full border-black overflow-hidden relative">
       <FlipLinks>
-        <div className="font-satoshi text-lg font-light sm:text-xl lg:text-xl  2xl:text-2xl">
-          {button.title}
+        <div className="px-5 py-2 flex items-center justify-center gap-2">
+          <div className="font-satoshi text-lg font-light sm:text-xl lg:text-xl  2xl:text-2xl">
+            {button.title}
+          </div>
+          <img src={button.icon} alt="" />
         </div>
-        <img src={button.icon} alt="" />
       </FlipLinks>
-    </motion.button>
+    </button>
   );
 };
 
