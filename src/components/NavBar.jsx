b3def51684df704a57280d392dc79f1fb9c621fc
Sam. <swayams24@gmail.com>
2024-10-20 00:30:18 +1100

    Navbar pointer default

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import FlipLinks from "./FlipLinks";
 const NavBar = () => {
   return (
     <nav className="flex justify-between p-8 items-center relative z-10">
-      <div className="font-cabinet font-light text-3xl max-sm:text-xl max-lg:text-2xl max-2xl:text-3xl 2xl:text-4xl">
+      <div className="font-cabinet font-light text-3xl max-sm:text-xl max-lg:text-2xl max-2xl:text-3xl 2xl:text-4xl cursor-default">
         samplestudio.
       </div>
       <div className="hidden max-sm:block">
