bdd52b5481dca610dd83cce5ce4f263c1c9546ca
Sam. <swayams24@gmail.com>
2024-10-21 22:03:36 +1100

    Add lenis Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import Lenis from "lenis";
 import About from "./sections/About";
 import FeaturedProjects from "./sections/FeaturedProjects";
 import Hero from "./sections/Hero";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="overflow-hidden">
       <Hero />
