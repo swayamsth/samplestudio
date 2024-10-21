701312cf3cb03aded08db31517477150977d6cdb
Sam. <swayams24@gmail.com>
2024-10-21 21:04:28 +1100

    Responsive grid, still big

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,43 +2,45 @@ import React from "react";
 
 const Grid = ({ projectItems }) => {
   return (
-    <div className="px-8 grid grid-cols-13 grid-rows-4 gap-8">
-      <div className="row-start-1 row-span-2 col-start-1 col-span-8">
-        <img
-          src={projectItems[0].image}
-          alt={projectItems[0].title}
-          className="object-cover rounded-3xl w-full h-full"
-        />
+    <section className="overflow-hidden px-8">
+      <div className="grid grid-cols-12 grid-rows-8 gap-6 md:gap-8 max-sm:flex max-sm:flex-col">
+        <div className="row-start-1 row-span-4 col-start-1 col-span-8">
+          <img
+            src={projectItems[0].image}
+            alt={projectItems[0].title}
+            className="object-cover rounded-3xl w-full h-full"
+          />
+        </div>
+        <div className="row-start-1 row-span-5 col-start-9 col-span-5">
+          <img
+            src={projectItems[1].image}
+            alt={projectItems[1].title}
+            className="object-cover rounded-3xl w-full h-full"
+          />
+        </div>
+        <div className="row-start-5 row-span-4 col-start-1 col-span-4">
+          <img
+            src={projectItems[2].image}
+            alt={projectItems[2].title}
+            className="object-cover rounded-3xl w-full h-full"
+          />
+        </div>
+        <div className="row-start-5 row-span-4 col-start-5 col-span-4">
+          <img
+            src={projectItems[3].image}
+            alt={projectItems[3].title}
+            className="object-cover rounded-3xl w-full h-full"
+          />
+        </div>
+        <div className="row-start-6 row-span-3 col-start-9 col-span-5">
+          <img
+            src={projectItems[4].image}
+            alt={projectItems[4].title}
+            className="object-cover rounded-3xl w-full h-full"
+          />
+        </div>
       </div>
-      <div className="row-start-1 row-span-3 col-start-9 col-span-5">
-        <img
-          src={projectItems[1].image}
-          alt={projectItems[1].title}
-          className="object-cover rounded-3xl w-full h-full"
-        />
-      </div>
-      <div className="row-start-3 row-span-2 col-start-1 col-span-4">
-        <img
-          src={projectItems[2].image}
-          alt={projectItems[2].title}
-          className="object-cover rounded-3xl w-full h-full"
-        />
-      </div>
-      <div className="row-stat-3 row-span-2 col-start-5 col-span-4">
-        <img
-          src={projectItems[3].image}
-          alt={projectItems[3].title}
-          className="object-cover rounded-3xl w-full h-full"
-        />
-      </div>
-      <div className="row-start-4 row-span-1 col-start-9 col-span-5">
-        <img
-          src={projectItems[4].image}
-          alt={projectItems[4].title}
-          className="object-cover rounded-3xl w-full h-full"
-        />
-      </div>
-    </div>
+    </section>
   );
 };
 
