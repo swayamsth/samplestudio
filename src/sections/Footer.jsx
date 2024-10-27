dcf9690e501cbb27ebe0f2a1dc15ae6b4ee6e208
Sam. <swayams24@gmail.com>
2024-10-27 22:53:41 +1100

    Margin changes on small screen ie mobile

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
   const socials = footer[2];
   return (
     <footer className="relative p-8">
-      <div className="flex justify-between">
+      <div className="flex justify-between max-sm:flex-col max-sm:gap-10">
         {/* Company Information */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-5 max-sm:gap-3">
           <h2 className="font-cabinet font-light text-4xl md:text-5xl xl:text-6xl">
             {companyInfo.companyName}
           </h2>
@@ -30,11 +30,11 @@ const Footer = () => {
         </div>
 
         {/* Quick Links */}
-        <div className="inline-flex flex-col gap-2 items-start">
+        <div className="inline-flex flex-col gap-6 items-start max-sm:gap-3">
           <h2 className="font-cabinet font-light text-3xl md:text-4xl xl:text-5xl">
             {quickLinks.title}
           </h2>
-          <div className="space-y-2">
+          <div className="space-y-6 max-sm:space-y-3">
             {quickLinks.links.map((link, index) => (
               <div className="flex">
                 <FlipLinks href={link.href} key={index}>
@@ -46,11 +46,11 @@ const Footer = () => {
         </div>
 
         {/* Socials */}
-        <div className="inline-flex flex-col gap-2 items-start">
+        <div className="inline-flex flex-col gap-6 items-start max-sm:gap-3">
           <h2 className="font-cabinet font-light text-3xl md:text-4xl xl:text-5xl">
             {socials.title}
           </h2>
-          <div className="space-y-2">
+          <div className="space-y-6 max-sm:space-y-3">
             {socials.links.map((link, index) => (
               <div className="flex">
                 <FlipLinks href={link.href} key={index}>
@@ -61,9 +61,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="mt-32 flex justify-between items-center gap-6">
+      <div className="mt-32 flex justify-between items-center gap-6 max-sm:gap-3 max-sm:mt-6">
         <div className="h-[2px] w-full bg-black rounded-full" />
-        <p className="font-satoshi font-light text-base max-lg:text-lg max-2xl:text-xl 2xl:text-2xl min-w-fit">
+        <p className="font-satoshi font-light text-base md:text-lg xl:text-xl 2xl:text-2xl min-w-fit">
           © 2024 samplestudio. All rights reserved.
         </p>
       </div>
