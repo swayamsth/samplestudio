419d49a2b84af790679b46cbd02ece95b7be2761
Sam. <swayams24@gmail.com>
2024-10-27 22:04:03 +1100

    Complete Footer layout

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,26 +9,63 @@ const Footer = () => {
   const socials = footer[2];
   return (
     <footer className="relative p-8">
-      {/* Company Information */}
-      <div>
-        <h2 className="">{companyInfo.companyName}</h2>
-        <p>{companyInfo.location}</p>
-        <p>{companyInfo.tel}</p>
-        <a href={`mailto:${companyInfo.email}`}>{companyInfo.email}</a>
-      </div>
-
-      {/* Quick Links */}
-      <div className="inline-flex flex-col gap-2 items-start">
-        <h2>{quickLinks.title}</h2>
-        <div className="space-y-2">
-          {quickLinks.links.map((link, index) => (
-            <div className="flex">
-              <FlipLinks href={link.href} key={index}>
-                {link.title}
-              </FlipLinks>
-            </div>
-          ))}
+      <div className="flex justify-between">
+        {/* Company Information */}
+        <div className="flex flex-col gap-4">
+          <h2 className="font-cabinet font-light text-4xl md:text-5xl xl:text-6xl">
+            {companyInfo.companyName}
+          </h2>
+          <p className="font-satoshi font-light text-2xl max-lg:text-xl max-2xl:text-2xl 2xl:text-3xl">
+            {companyInfo.location}
+          </p>
+          <p className="font-satoshi font-light text-2xl max-lg:text-xl max-2xl:text-2xl 2xl:text-3xl">
+            {companyInfo.tel}
+          </p>
+          <a
+            href={`mailto:${companyInfo.email}`}
+            className="font-satoshi font-light text-2xl max-lg:text-xl max-2xl:text-2xl 2xl:text-3xl"
+          >
+            {companyInfo.email}
+          </a>
         </div>
+
+        {/* Quick Links */}
+        <div className="inline-flex flex-col gap-2 items-start">
+          <h2 className="font-cabinet font-light text-3xl md:text-4xl xl:text-5xl">
+            {quickLinks.title}
+          </h2>
+          <div className="space-y-2">
+            {quickLinks.links.map((link, index) => (
+              <div className="flex">
+                <FlipLinks href={link.href} key={index}>
+                  {link.title}
+                </FlipLinks>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Socials */}
+        <div className="inline-flex flex-col gap-2 items-start">
+          <h2 className="font-cabinet font-light text-3xl md:text-4xl xl:text-5xl">
+            {socials.title}
+          </h2>
+          <div className="space-y-2">
+            {socials.links.map((link, index) => (
+              <div className="flex">
+                <FlipLinks href={link.href} key={index}>
+                  {link.title}
+                </FlipLinks>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="mt-32 flex justify-between items-center gap-6">
+        <div className="h-[2px] w-full bg-black rounded-full" />
+        <p className="font-satoshi font-light text-base max-lg:text-lg max-2xl:text-xl 2xl:text-2xl min-w-fit">
+          © 2024 samplestudio. All rights reserved.
+        </p>
       </div>
     </footer>
   );
