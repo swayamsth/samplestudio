61803b59c9a881b3bcfb0224b04b654680cdac0e
Sam. <swayams24@gmail.com>
2024-11-05 20:29:39 +1100

    Removed animation

--- src/sections/FeaturedProjects.jsx
+++ src/sections/FeaturedProjects.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { projects } from "../constants";
 import Button from "../components/Button";
 import Grid from "../components/Grid";
-import { fadeIn } from "../constants/anim";
-import { motion } from "framer-motion";
 
 const FeaturedProjects = () => {
   const projectItems = projects.slice(0, 5);
@@ -11,30 +9,18 @@ const FeaturedProjects = () => {
 
   return (
     <section>
-      <motion.div
-        variants={fadeIn("down", 0.2)}
-        initial="hidden"
-        whileInView={"visible"}
-        viewport={{ once: true }}
-        className="flex items-center gap-8 p-6 sm:p-7 lg:p-8"
-      >
+      <div className="flex items-center gap-8 p-6 sm:p-7 lg:p-8">
         <h1 className="font-cabinet text-3xl font-medium sm:text-4xl lg:text-5xl 2xl:text-6xl min-w-fit">
           Featured Projects
         </h1>
         <div className="h-[2px] w-full bg-black rounded-full" />
-      </motion.div>
+      </div>
 
       <Grid projectItems={projectItems} />
 
-      <motion.div
-        variants={fadeIn("up", 0.2)}
-        initial="hidden"
-        whileInView={"visible"}
-        viewport={{ once: true }}
-        className="my-8 flex items-center justify-center"
-      >
+      <div className="my-8 flex items-center justify-center">
         {exploreButton && <Button button={exploreButton} />}
-      </motion.div>
+      </div>
     </section>
   );
 };
