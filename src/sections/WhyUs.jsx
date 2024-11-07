3373971c19d63ec16f49d94f3710d0caad910b6f
Sam. <swayams24@gmail.com>
2024-11-07 21:01:18 +1100

    Add opcaity animation to Description

--- src/sections/WhyUs.jsx
+++ src/sections/WhyUs.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { whyUsHeading } from "../constants";
 import SetApart from "../components/SetApart";
 import { motion } from "framer-motion";
+import { fadeIn } from "../constants/anim";
 
 const WhyUs = () => {
   const [isHovered, setHovered] = useState(false);
@@ -12,14 +13,29 @@ const WhyUs = () => {
           key={index}
           className="p-8 flex justify-between max-sm:flex-col max-sm:gap-3"
         >
-          <h1 className="font-cabinet text-3xl font-medium sm:text-4xl lg:text-5xl 2xl:text-6xl w-[8ch]">
+          <motion.h1
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+            className="font-cabinet text-3xl font-medium sm:text-4xl lg:text-5xl 2xl:text-6xl w-[8ch]"
+          >
             {title}
-          </h1>
+          </motion.h1>
 
           <div className="flex flex-col gap-5 justify-start items-start md:max-w-[35ch]  lg:max-w-prose">
-            <p className="font-satoshi font-light text-base sm:text-lg lg:text-xl 2xl:text-2xl">
+            <motion.p
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                ease: [0.65, 0, 0.35, 1],
+              }}
+              viewport={{ once: true }}
+              className="font-satoshi font-light text-base sm:text-lg lg:text-xl 2xl:text-2xl"
+            >
               {description}
-            </p>
+            </motion.p>
 
             <motion.button
               onMouseEnter={() => setHovered(true)}
