e04dab913308660634fe0d107fa868fb047036f7
Sam. <swayams24@gmail.com>
2024-11-08 01:34:51 +1100

    Add toggle animation for menu on mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { Children, useState } from "react";
 import FlipLinks from "./FlipLinks";
 import Magnetic from "./Magnetic";
-import { motion } from "framer-motion";
-import { fadeIn } from "../constants/anim";
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  fadeIn,
+  menuVars,
+  mobileLinksVars,
+  containerVars,
+} from "../constants/anim";
+
+const navLinks = [
+  { title: "about us", href: "#" },
+  { title: "projects", href: "#" },
+  { title: "service", href: "#" },
+  { title: "contact", href: "#" },
+];
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <nav className="flex justify-between p-8 items-center relative z-10">
       <Magnetic>
@@ -18,15 +32,60 @@ const NavBar = () => {
           samplestudio.
         </motion.div>
       </Magnetic>
+
+      {/* Mobile Menu */}
       <motion.div
         variants={fadeIn("down", 2.4)}
         initial="hidden"
         animate={"visible"}
         viewport={{ once: true }}
         className="hidden max-sm:block"
+        onClick={toggleMenu}
       >
         <img src="./icons/menu.svg" alt="" />
       </motion.div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            variants={menuVars}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed left-0 top-0 w-full h-screen origin-top bg-[#141516] p-8"
+          >
+            <div className="flex h-full flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-white text-xl">samplestudio.</h1>
+                <div onClick={toggleMenu}>
+                  <img src="./icons/x.svg" alt="" className="cursor-pointer" />
+                </div>
+              </div>
+
+              <motion.div
+                variants={containerVars}
+                initial="initial"
+                animate="open"
+                exit="initial"
+                className="flex flex-col h-full justify-center font-cabinet items-center gap-4"
+              >
+                {navLinks.map((link, index) => (
+                  <div className="overflow-hidden">
+                    <MobileNavLink
+                      title={link.title}
+                      href={link.href}
+                      key={index}
+                    />
+                  </div>
+                ))}
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* Mobile menu end */}
+
       <motion.div
         variants={fadeIn("down", 2.4)}
         initial="hidden"
@@ -44,3 +103,11 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+const MobileNavLink = ({ title, href }) => {
+  return (
+    <motion.div variants={mobileLinksVars}>
+      <a className="text-white text-5xl">{title}</a>
+    </motion.div>
+  );
+};
--- src/constants/anim.jsx
+++ src/constants/anim.jsx
@@ -1,3 +1,5 @@
+import { delay, stagger } from "framer-motion";
+
 export const fadeIn = (direction, delay) => ({
   hidden: {
     opacity: 0,
@@ -27,5 +29,59 @@ export const scale = {
   transition: {
     duration: 0.5,
     ease: [0.65, 0, 0.35, 1],
+  },
+};
+
+export const menuVars = {
+  initial: {
+    scaleY: 0,
+  },
+  animate: {
+    scaleY: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.12, 0, 0.39, 0],
+    },
+  },
+  exit: {
+    scaleY: 0,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+};
+
+export const mobileLinksVars = {
+  initial: {
+    y: "30vh",
+    transition: {
+      duration: 0.5,
+      ease: [0.37, 0, 0.63, 1],
+    },
+  },
+  open: {
+    y: 0,
+    transition: {
+      duration: 0.7,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+};
+
+export const containerVars = {
+  initial: {
+    transition: {
+      staggerChildren: 0.09,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.09,
+      staggerDirection: 1,
+    },
   },
 };
